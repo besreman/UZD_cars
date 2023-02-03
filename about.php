182bdc3227798896d1cab66ef7844dd3dde128cb
Minalu <65035577+besreman@users.noreply.github.com>
2023-02-03 21:47:59 +0300

    create about.php file

---
+++ about.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width" ,initial-scale=1.0">
+    <title>UZD cars: about us</title>
+
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+
+
+    <!-- external css file linking-->
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+
+
+
+
+
+
+    <!--header starting line-->
+
+
+    <section class="header">
+
+
+        <img src="images/logo1.png" alt="logo" id="logo">
+        <!--<a href="home.php" class="logo">UZD cars Ethiopia</a>-->
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="lists.php">cars</a>
+            <a href="about.php">about us</a>
+
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <!--header ending line-->
+
+
+
+    <div class="heading">
+        <h1>about us</h1>
+    </div>
+
+
+    <!-- about Us main section starts here-->
+
+    <section class="about">
+        <div class="image">
+            <img src="images/logo1.png" alt="UZD_cars logo">
+        </div>
+        <div class="content">
+            <h3>why UZD_cars?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Itaque aperiam corporis amet possimus consequatur ea facilis
+                laboriosam. Voluptatem ea provident sunt tenetur quasi amet hic
+                dignissimos, sapiente natus commodi quia.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Dicta neque dolor, eos atque eaque velit quis? Temporibus ex fugit esse.</p>
+            <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-map"></i>
+                    <span>health and good quality cars</span>
+                </div>
+
+                <div class="icons">
+                    <i class="fas fa-hand-holding-usd"></i>
+                    <span>affordable price rate</span>
+                </div>
+
+                <div class="icons">
+                    <i class="fas fa-headset"></i>
+                    <span>24hr services</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- about Us main section ends here -->
+
+    <!-- review section start-->
+
+    <section class="reviews">
+
+        <div class="swiper reviews-slider">
+
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Ad animi tempora doloribus numquam nisi voluptatem! Sint
+                        laborum voluptas voluptatem molestias illo est eos,
+                        exercitationem, doloribus consequuntur eveniet officia architecto officiis.</p>
+                    <h3>Mr Ayele Woldekidan</h3>
+                    <span>private investor</span>
+                    <img src="images/pm1.jpg" alt="ayeles pic">
+                </div>
+
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Ad animi tempora doloribus numquam nisi voluptatem! Sint
+                        laborum voluptas voluptatem molestias illo est eos,
+                        exercitationem, doloribus consequuntur eveniet officia architecto officiis.</p>
+                    <h3>Mr Wesen Mengesha</h3>
+                    <span>private investor</span>
+                    <img src="images/pm3.jpg" alt="Wesen pic">
+                </div>
+
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Ad animi tempora doloribus numquam nisi voluptatem! Sint
+                        laborum voluptas voluptatem molestias illo est eos,
+                        exercitationem, doloribus consequuntur eveniet officia architecto officiis.</p>
+                    <h3>Mrs Selamawit Jemberu</h3>
+                    <span>private investor</span>
+                    <img src="images/pw1.jpg" alt="ayeles pic">
+                </div>
+
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Ad animi tempora doloribus numquam nisi voluptatem! Sint
+                        laborum voluptas voluptatem molestias illo est eos,
+                        exercitationem, doloribus consequuntur eveniet officia architecto officiis.</p>
+                    <h3>Mr Mintesnot Mesele</h3>
+                    <span>private investor</span>
+                    <img src="images/pm4.jpg" alt="Mintesnot pic">
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <!-- review section end-->
+
+
+
+
+
+
+
+
+
+
+
+    <!--footer start-->
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>Shortcuts</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="categories.php"><i class="fas fa-angle-right"></i>categories</a>
+                <a href="buy.php"><i class="fas fa-angle-right"></i>buy now!</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about us</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+                <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+            </div>
+
+            <div class="box">
+                <h3>contact us</h3>
+                <a href="#"><i class="fas fa-phone"></i>+123-456-7890</a>
+                <a href="#"><i class="fas fa-phone"></i> +111-222-3333</a>
+                <a href="#"><i class="fas fa-envelope"></i>besratmesele.com</a>
+                <a href="#"><i class="fas fa-map"></i>addis ababa, Ethiopia-400104</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us </h3>
+                <a href="#"><i class="fab fa-facebook"></i> facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>instagram </a>
+                <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit">Designed by <span>MY-square developers</span>| all rights reserved!</div>
+
+    </section>
+
+    <!--footer end-->
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+    <!-- custom javascript file-->
+    <script src="script.js"></script>
+</body>
